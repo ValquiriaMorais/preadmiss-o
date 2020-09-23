09b3599739e957431a38b60c657504dcfda7cd4e
Valquíria Morais <71649464+ValquiriaMorais@users.noreply.github.com>
2020-09-23 17:38:26 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+const name = prompt("Olá, bem vinda(o)! Qual o seu nome?");
+let question = prompt("Coloque o número correspondente a sua resposta \nQuer iniciar o teste? \n1.Sim \n2.Não");
+
+if(question<2){
+   let firtQuestion = prompt("Quanto é 2 + 2? \n1. 4 \n2. 5 \n3. 6");
+   let secondQuestion = prompt("Quanto é 200/4? \n1. 50 \n2. 100 \n3. 80");
+   let thirdQuestion = prompt("Quanto é 20*5? \n1. 100 \n2. 50 \n3. 200");
+}else{
+    alert("Obrigada!");
+}
